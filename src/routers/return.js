653aae4c720ff1aff006aece2ec99fa1b3fe4faf
michael1017 <michael881017@gmail.com>
2021-06-25 10:17:30 +0000

    fix return.js

--- src/routers/return.js
+++ src/routers/return.js
@@ -7,17 +7,16 @@ const router = express.Router();
 router.use(bodyParser.json());
 
 //To be verified
-router.get('/receive', function (req, res) {
+router.get('/', function (req, res) {
   const { id } = req.body;
   const data_prefix = '../data/';
-  const filename = data_prefix + id;
+  const filename = data_prefix + id; // TODO: change 0 to id
   const filename_svg = filename + '.svg';
+  console.log("return.js submit " + filename_svg);
   fs.readFile(filename_svg, function(err, data) {
     if (err) throw err // Fail if the file can't be read.
     res.writeHead(200, {'Content-Type': 'image/svg+xml'});
-    res.write('"data:image/svg+xml;utf8,');
-    res.write(Buffer.from(data).toString('utf8'));
-    res.end('"');
+    res.end(Buffer.from(data).toString('utf8'));
   })
 });
 
