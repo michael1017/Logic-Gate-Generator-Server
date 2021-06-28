f2310adb052bbe066b0c2e7366a7fd4c1a7a35b7
michael1017 <michael881017@gmail.com>
2021-06-28 17:28:42 +0000

    fix svg, py

--- generator.py
+++ generator.py
@@ -11,15 +11,16 @@ args = parser.parse_args()
 
 assert(args.input.endswith('.v'))
 output_file = args.input[:-2]
-print(output_file, args.top)
+topModule = args.top.strip()
+print(output_file, topModule)
 output_v    = output_file + '.v'
 output_json = output_file + '.json'
 output_svg  = output_file + '.svg'
 identifier =  re.compile('[a-zA-Z_]+[a-zA-Z0-9_]*')
-assert(identifier.match(args.top))
+assert(identifier.match(topModule))
 
 # yosys -p "prep -top top_module_name; write_json output.json" sample.v
-prep_command = '"prep -top ' + args.top.strip() + '; write_json ' + output_json + '"'
+prep_command = '"prep -top ' + topModule + '; write_json ' + output_json + '"'
 yosys_command = ['yosys', '-p', prep_command, output_v]
 # netlistsvg test1.json -o out.svg
 netlistsvg_command = ['netlistsvg', output_json, '-o', output_svg, "--skin", "default.svg"]
